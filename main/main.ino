ef75903d72061c11bb92bdf05867b4fe766f9cd2
Henrique Albuquerque <henriquemoreira842@gmail.com>
2024-06-16 20:53:39 -0300

    Implementação do pwm no led UV

--- main/main.ino
+++ main/main.ino
@@ -1,74 +1,29 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include <Arduino.h>
 
+#define pin1 17
+#define pin2 16
+#define pin3 27
 
-
-String URL = "http://192.168.1.227/teste/server.php";
-
-const char* ssid = "LESC"; 
-const char* password = "123456789"; 
-
-int temperature = 0;
-int humidity = 0;
+int i = 0;
 
 void setup() {
-  Serial.begin(115200);
-
-  
-  connectWiFi();
+  pinMode(pin1, INPUT_PULLUP);
+  pinMode(pin2, INPUT_PULLUP);
+  pinMode(pin3, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if(WiFi.status() != WL_CONNECTED) {
-    connectWiFi();
+  //Serial.print("teste");
+  if(digitalRead(pin1) == LOW && i < 255){
+    i++;
+    Serial.print(String(i) + "\n");
+    analogWrite(pin3, i);
+    delay(10);
+  } else if(digitalRead(pin2) == LOW && i > 0){
+    i--;
+    Serial.print(String(i) + "\n");
+    analogWrite(pin3, i);
+    delay(10);
   }
-
-  String postData = "temperature=" + String(temperature) + "&humidity=" + String(humidity);
-  
-  HTTPClient http;
-  http.begin(URL);
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  
-  int httpCode = http.POST(postData);
-  String payload = "";
-
-  if(httpCode > 0) {
-    // file found at server
-    if(httpCode == HTTP_CODE_OK) {
-      String payload = http.getString();
-      Serial.println(payload);
-    } else {
-      // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-    }
-  } else {
-    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-  }
-  
-  http.end();  //Close connection
-
-  Serial.print("URL : "); Serial.println(URL); 
-  Serial.print("Data: "); Serial.println(postData);
-  Serial.print("httpCode: "); Serial.println(httpCode);
-  Serial.print("payload : "); Serial.println(payload);
-  Serial.println("--------------------------------------------------");
-  delay(5000);
-}
-
-void connectWiFi() {
-  WiFi.mode(WIFI_OFF);
-  delay(1000);
-  //This line hides the viewing of ESP as wifi hotspot
-  WiFi.mode(WIFI_STA);
-  
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting to WiFi");
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-    
-  Serial.print("connected to : "); Serial.println(ssid);
-  Serial.print("IP address: "); Serial.println(WiFi.localIP());
 }
